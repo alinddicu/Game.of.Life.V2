58760c3ea69757835c732b76885bac297b4aa35c
alinddicu <alinddicu@yahoo.com>
2014-11-19 10:02:38 +0100

    Init

---
+++ Game.of.Life.V2/Game.of.Life.V2.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Game.of.Life.V2.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
